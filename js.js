1bc2582290e8ad2e3db151a0be83be1dd1227b5e
srini88 <pvsrinivas.ece@gmail.com>
2016-05-19 15:14:47 -0500

    more

--- js.js
+++ js.js
@@ -4,17 +4,29 @@ $(document).ready(function(){
  	//$('tr:even').css("backgroundColor", "green");  //all even ones..
 	$('tr:last-child').css("backgroundColor", "green");   //last one
 
-	console.log($('div:contains("basha")').html());  //manik basha.s.dfsd
+	console.log($('div:contains("basha")').html("updated Shit"));    //prints updated Shit..
 
-	//if manik basha is in another div .html would print <div>manik basha.s.dfsd</div>
+	///get the html or update the html
 
 });
 
+///have to master selectors is the key to being productive with jQuery..
 
-//$('#datatable tr:odd')   ...on a particular table...very specific selector...
+//check this outhttp://www.codylindley.com/jqueryselectors/
 
-// $('div[title$="Title"]') //ends with Title..
-// ^ starts with 
-// * contains ...
+//li:first
+
+//li:nth-child(2)
+
+// first div 
+
+// div:eq(0)
+
+$('code') all code tags...
 
 
+$('div code')  - code inside a div..
+
+
+The child combinator (E > F) can be thought of as a more specific form of the descendant combinator (E F) in that it selects only first-level descendants
+documentation$('li > ul')toggle
