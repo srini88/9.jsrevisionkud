fa08c7ebc1f897e59600252adf06e8c6c5794568
srini88 <pvsrinivas.ece@gmail.com>
2016-05-19 15:36:08 -0500

    more.

--- js.js
+++ js.js
@@ -1,22 +1,14 @@
 $(document).ready(function(){  
 
+	$('div').each(function(index){
 
-	//$('ul').css("backgroundColor","red");  //will set to everything
-	//$('li').css("backgroundColor","red");  //will set to everything but portion before . will be cut off --very imp
-
-	$('#myDiv').css("backgroundColor","red");  //Selects all direct child elements specified by "child" of elements specified by "parent".
-
-	//DRIECT child
+		console.log(index +" = "+ $(this).text())  //includes the parent div as well
+	})
 
 });
 
 
-
-// $('code') all code tags...
-
-
-// $('div code')  - code inside a div..
+//.each(function(index, Element)) is used to iterate through jQuery objects:
 
 
-// The child combinator (E > F) can be thought of as a more specific form of the descendant combinator (E F) in that it selects only first-level descendants
-// documentation$('li > ul')toggle
+///the 2nd parameter element is just this..
