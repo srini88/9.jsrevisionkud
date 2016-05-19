3e366c2a1d79b07a42bcdbf3b692aa12c695dc6f
srini88 <pvsrinivas.ece@gmail.com>
2016-05-19 10:11:58 -0500

    onload vs docready

--- js.js
+++ js.js
@@ -1,31 +1,9 @@
-$(function(){  //shorthand syntam  $(document).ready(function())
 
-	var div1= $('#div1');
-	var div2 = $('#div2');
+window.onload = function(){
+	console.log("window loaded");
+}
 
-	function1 = function(){   //global
-		div1.append('function1 executed <br/>');
-	}
-	var	function2 = function(){  // only within this doc.ready
-		div1.append('function2 executed <br/>');
-	}
-	var obj = {
-		function3 : function(){
-			div1.append('function3 executed <br/>');
-		}
-	}
-	gobj = {
-		function4 : function(){
-			div1.append ('function4 executed <br/>');
-		}
-	}
-	function1();
-	function2();
-	obj.function3();
-	gobj.function4();
-	div1.append("<br/>");
+$(document).ready(function(){  ///it does not wait for your images and all shit...  you would see this printing out..
+	console.log("DOM loaded...hierarchy...");
+});
 
-	div2.load('BasicFn.html');
-
-
-})
--- notes.js
+++ notes.js
@@ -7,3 +7,49 @@ silver square with a black logo
 
 jquery dan Wahlin... great course.....Jquery fundamentals..
 
+why use jquery?
+
+single javascript file.... very powerful,. cross browser....amazing...I6, firefox, chrome, opera...
+
+jquery makes it you can focus on your app... jquery can take care for you...
+
+featuers -- selectors.....imp...
+events --- imps..
+very rich AJAX --- use jquery to callback to server and get JSON...
+and finally plugins...very rich free jQuery plugins..
+
+how do you locate elements with a specific class? modern browser use query 
+
+how do you apply styles to multiple elements?
+
+how do you handle events in a cross browser mannner?
+
+IE 6-8   go with jQuery 1.x 
+
+also jQuery 2.x 
+
+
+why CDN?  go to a site -- and they use this URL from google...that script is cached for that domain..
+
+you go to another domain ..it is already cached... 
+
+microsoft and google have data centers all over the world.. get some speed benefits.. 
+caching, regionality..
+parallelism..
+
+what happens if you can't get jQuery script... will cause a big problem..
+
+provide a fall back ...
+
+<script src ="cnd link"></script>  successful window object will have  anew property called jQuery 
+<script>
+window.jQuery || document.write('<script src = "jquery"></\script>');
+</script>
+
+$ = is the alias.. called once DOM hierarcy is loaded...but before all  the images have loaded.....
+
+when it is ready ,,call the callback --- inline anonymous function..
+
+use $(document).ready(function(){
+
+});
