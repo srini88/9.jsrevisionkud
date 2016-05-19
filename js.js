2d4963b7e849b2d1d85713c23390735903930d60
srini88 <pvsrinivas.ece@gmail.com>
2016-05-19 10:51:25 -0500

    color green

--- js.js
+++ js.js
@@ -1,19 +1,19 @@
-
-
-//html taking a function and returning content...
-
-
-
 $(document).ready(function(){  
-	$('div').html(function(){
-		
-
-		return "<b>Wow</b> such excitement..";
-	}); 
-	//console.log($('div b')); //returns a collection..
-
-	$('div b').append(document.createTextNode("!!!")).css("color", "red");
+	$('div').css("color", "green"); //changes the text color to green
+	//$('div').css("backgroundColor", "green");  //changes the backgroudn color
 	
 });
 
-//2 divs .. you get 2 as length.. 
+///the above div we dont have to loop through to apply the styles... we we dont hav to ..
+
+
+
+
+// $('p') selects all <p> elemtns ..
+// $('a') se;ects all <a> elemetns..
+
+// $('p, a, span') selects all paragrapshs, anchor and span elemetns. 
+
+// $('table tr')  selects all tr elements that are descendants of the table element...
+
+// Descendants are children, grandchilderen etc of the designated ancestor elemetn. 
