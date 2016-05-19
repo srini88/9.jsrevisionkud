68eea9edae18a29b810d89c0d4486da8ea1c6fd0
srini88 <pvsrinivas.ece@gmail.com>
2016-05-19 14:06:50 -0500

    more

--- js.js
+++ js.js
@@ -1,22 +1,18 @@
 $(document).ready(function(){  
 
 
+ 	
 
- var inputs = $(':input');  
-
-
- inputs.each(function(){
- 	var element = $(this);   //putting jQuery wrapper..
- 	//element.val("Hu");  ///changes all values to "HU"
- 	console.log(element.val());
- });
+ 	$('span:first-child').css("backgroundColor", "red")
 
 
-//would print the selected one in the options..
-//if selected is not chosen...it will just default to the first one..
-
+ 
 });
 
-//also $('form :input') , $('#form1 :input')
+
+// Selecting the first child 
+
+// $('element:first-child') selects the first child of every element group..
 
 
+// $('span:first-child')
