9a1629e193f04463beef5d064dfce9802b396c08
srini88 <pvsrinivas.ece@gmail.com>
2016-05-19 15:22:53 -0500

    more

--- js.js
+++ js.js
@@ -1,32 +1,22 @@
 $(document).ready(function(){  
 
-	//$('tr').css("backgroundColor", "green");  //all rows greeen
- 	//$('tr:even').css("backgroundColor", "green");  //all even ones..
-	$('tr:last-child').css("backgroundColor", "green");   //last one
 
-	console.log($('div:contains("basha")').html("updated Shit"));    //prints updated Shit..
+	//$('ul').css("backgroundColor","red");  //will set to everything
+	//$('li').css("backgroundColor","red");  //will set to everything but portion before . will be cut off --very imp
 
-	///get the html or update the html
+	$('#myDiv').css("backgroundColor","red");  //Selects all direct child elements specified by "child" of elements specified by "parent".
+
+	//DRIECT child
 
 });
 
-///have to master selectors is the key to being productive with jQuery..
-
-//check this outhttp://www.codylindley.com/jqueryselectors/
-
-//li:first
-
-//li:nth-child(2)
-
-// first div 
-
-// div:eq(0)
-
-$('code') all code tags...
 
 
-$('div code')  - code inside a div..
+// $('code') all code tags...
 
 
-The child combinator (E > F) can be thought of as a more specific form of the descendant combinator (E F) in that it selects only first-level descendants
-documentation$('li > ul')toggle
+// $('div code')  - code inside a div..
+
+
+// The child combinator (E > F) can be thought of as a more specific form of the descendant combinator (E F) in that it selects only first-level descendants
+// documentation$('li > ul')toggle
