d0294c8b308061a31b8ec2f40bf6f9667507184a
srini88 <pvsrinivas.ece@gmail.com>
2016-05-19 10:18:43 -0500

    .html get

--- js.js
+++ js.js
@@ -1,9 +1,14 @@
+// When this method is used to return content, it returns the content of the FIRST matched element.
 
-window.onload = function(){
-	console.log("window loaded");
-}
+// When this method is used to set content, it overwrites the content of ALL matched elements.
+
+This method uses the browser's innerHTML property...
+
 
 $(document).ready(function(){  ///it does not wait for your images and all shit...  you would see this printing out..
-	console.log("DOM loaded...hierarchy...");
+	//document.getElementById
+	var boo = $('div.demo-container').html();  //
+	console.log(boo); //<div class="demo-box">Demonstration Box</div>
+
 });
 
