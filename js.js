8fa4ad98264446de6fe8e72a76a58bfe3c820014
srini88 <pvsrinivas.ece@gmail.com>
2016-05-20 12:12:38 -0500

    mouseup

--- js.js
+++ js.js
@@ -4,3 +4,11 @@
 
 // Note: For select menus, the change event occurs when an option is selected. For text fields or text areas, the change event occurs when the field loses focus, after the content has been changed.
 
+
+// With a mouseup event, you can click somewhere else on the screen, hold down the click button, and move the pointer to your mouseup element, and then release the mouse pointer.
+
+// A click event requires the mousedown and mouseup event to happen on that element.
+
+// The normal expectation is that a click requires both the mousedown and mouseup event, so I'd recommend the click event.
+
+// From the possible duplicate, it appears that mouseup and mousedown events can also be caused by mouse buttons other than the left click button. Which is very different from what a generic user would expect.
