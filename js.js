c9d0855cf06aef45494d6015f44e789fa3c7a486
srini88 <pvsrinivas.ece@gmail.com>
2016-05-19 20:22:14 -0500

    onfocus vs onblur toggleclass

--- js.js
+++ js.js
@@ -2,16 +2,24 @@ $(document).ready(function(){
 
 
 
-var selc = $("div div:nth-child(3)");
-	selc.addClass('Highlight')   //will add to the third div..
+var selc = $("div div");
+	selc.toggleClass('Highlight')   //will add to the third div..
 	//also can use first last 
 	
 
 
+
 });
+
+function myFocus(e){
+	console.log(e);
+	$(e).toggleClass('Highlight');
+}
+
 
 
 ///.toggleClass() alternates adding or removing a class based on the current presence or absence of the class...
 
 
 
+// As you know, the onBlur event fires for an element if that element had the focus, but loses it.
