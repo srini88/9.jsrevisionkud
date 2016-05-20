6dfe859fdb84bcf887f84f5d5a706fc10db3b3a7
srini88 <pvsrinivas.ece@gmail.com>
2016-05-19 19:08:39 -0500

    styles

--- js.js
+++ js.js
@@ -1,11 +1,17 @@
 $(document).ready(function(){  
-	$('.boo').wrap('<div class="parent" />');
-	$('div.parent').remove();
+
+
+
+	$("div").css({
+		'color' :'#ccc',
+		'font-weight':'bold'
+	}  )//.text("chaged color");              //tag selector all divs
+
 
 
 });
 
-//remove node...
-///not only removes the div but also the content inside it...the child stuff because they have no where to go..
+//do it the above way,,, also chain one property at a time..
 
-// other way with vanilla JS -- find the node first...and then remove it from it'sparent...find the parent also
+
+//by adding .text it removes what is there before and appends chaged color
