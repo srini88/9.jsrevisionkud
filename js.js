eb2dd199e624ae9d737c4a4848f03c0d95c185e9
srini88 <pvsrinivas.ece@gmail.com>
2016-05-19 19:31:28 -0500

    more

--- js.js
+++ js.js
@@ -2,47 +2,16 @@ $(document).ready(function(){
 
 
 
-var selc = $("div div");
+var selc = $("div div:nth-child(3)");
+	selc.addClass('Highlight')   //will add to the third div..
+	//also can use first last 
+	
 
-	selc.each(function(index, element){
-
-		$(this).addClass('class' + index);  //added class0, class1 ..etc..
-
-	})
-
-	selc.each(function(index, element){
-		if ($(this).hasClass('class3')){
-			$(this).addClass('newshit')
-		}
-	})
-
-	setInterval(function(){
-		selc.removeClass();
-	}, 2000);    ///removing all classes after two seconds...
-
-	setInterval(function(){
-		selc.addClass("hi");
-	}, 2100); 
-
-	//adding and removing classes periodically...
-
-	////it will add newshit class to the class that contains class3..
 
 });
 
-//output will be same class for each div
 
-// selc.addClass('classOne classTwo');
-
-
-// based on hasClass is true or flase, you can add more functionality...
+///.toggleClass() alternates adding or removing a class based on the current presence or absence of the class...
 
 
 
-// .removeClass  can remove one or more classes: 
-// $('p').removeClass('classOne classTwo')
-
-
-// remove all class attributes for the matching selector..
-
-// $('p').removeClass(); 
